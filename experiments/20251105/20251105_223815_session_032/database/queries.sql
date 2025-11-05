fe519ea8d3ad59fa4bbd685e2e424428bf804105
ieyeppo <ieyeppo.job@gmail.com>
2025-11-05 23:12:37 +0900

    feat: 실험 폴더 추가

---
+++ experiments/20251105/20251105_223815_session_032/database/queries.sql
@@ -0,0 +1,13 @@
+-- 실행 시간: 2025-11-05 22:43:41
+                            -- 도구: 
+                            -- 설명: 
+                        -- 파라미터: ('%"Attention Is All You Need"%',)
+-- 결과 개수: 0
+
+
+        SELECT paper_id, title, authors, abstract, publish_date
+        FROM papers
+        WHERE title ILIKE %s
+        LIMIT 1
+        ;
+
